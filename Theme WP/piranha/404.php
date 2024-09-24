05e14316bd4cdb3a83b2481bcb4b518587e20f1a
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-09-24 19:37:38 +0200

    Update 404.php

--- Theme WP/piranha/404.php
+++ Theme WP/piranha/404.php
@@ -1,1 +1,25 @@
-<?php wp_redirect(home_url()) ?>
+<?php get_header(); ?>
+
+    <section class="block-404">
+        <div class="container-fluid text-center fade-in-wrapper">
+
+            <?php if(get_field('title_404', 'options')):?>
+                <h1><?php the_field('title_404', 'options');?></h1>
+            <?php endif;?>
+
+            <?php $link = get_field('link_404', 'options');
+
+            if( $link ):
+                $link_url = $link['url'];
+                $link_title = $link['title'];
+                $link_target = $link['target'] ? $link['target'] : '_self';
+                ?>
+                <a class="btn btn-dark" href="<?= esc_url($link_url); ?>" target="<?= esc_attr($link_target); ?>">
+                    <span data-text="<?= esc_html($link_title); ?>"><?= esc_html($link_title); ?></span>
+                </a>
+            <?php endif; ?>
+
+        </div>
+    </section>
+
+<?php get_footer();
