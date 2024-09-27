f8f0d19c7d5007dc5754a9ccbc791090130d6468
Stanislav-pr <stanislav.retinskiy@gmail.com>
2024-09-27 14:42:54 +0200

    del

--- Theme WP/piranha/functions.php
+++ Theme WP/piranha/functions.php
@@ -4,8 +4,6 @@ include 'inc/enqueue.php';     // add styles and scripts
 include 'inc/acf.php';         // custom acf functions
 include 'inc/extras.php';      // custom functions
 include 'classes/walker.php';  // walker nav menu
-include 'inc/register.php';    // custom ajax register and auth
-include 'inc/ajax-actions.php';// ajax actions
 
 
 add_action('after_setup_theme', 'theme_register_nav_menu');
